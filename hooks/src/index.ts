84bb94eeb663031ebbf5347ae635ef755f70da61
akash patel <akash906129@gmail.com>
2025-02-28 18:56:22 +0530

    changed schemma

--- hooks/src/index.ts
+++ hooks/src/index.ts
@@ -1,8 +1,35 @@
 import express from "express";
+import { PrismaClient } from "@prisma/client";
+const client = new PrismaClient;
 const app = express();
 
-app.post("/hook/catch/:userId/:zapId", (req, res) => {
+app.post("/hook/catch/:userId/:zapId", async (req, res) => {
     const userId = req.params.userId;
     const zapId = req.params.zapId;
-    
+    const body = req.body;
+
+    await client.$transaction(async tx => {
+
+        const run = await client.zapRun.create({
+            data: {
+                zapId: zapId,
+                metadata: body,
+            }
+        })
+
+        await client.zapRunOutbox.create({
+            data: {
+                zapRunId: run.id,
+            }
+        })
+    })
+
+    res.json({ msg: "webhook recived" })
+
+
 })
+
+app.listen(3000, () => [
+    console.log("app is on " + "http://localhost:3000")
+
+])
