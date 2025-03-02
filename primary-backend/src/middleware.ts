cd3b2d99c563c219ca5e38efbf0790b226569bcf
akash patel <akash906129@gmail.com>
2025-03-02 16:12:20 +0530

    authheader middleware

--- primary-backend/src/middleware.ts
+++ primary-backend/src/middleware.ts
@@ -1,3 +1,25 @@
-export function authMiddleware() { 
-    
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken"
+import { JWT_PASSWORD } from "./config";
+
+
+export function authMiddleware(req: Request, res: Response, next: NextFunction) {
+    // @ts-ignore
+    const token = req.headers.authorization as unknown as string;
+    try {
+        const payload = jwt.verify(token, JWT_PASSWORD);
+        if (payload) {
+            // @ts-ignore
+            req.id = payload.id
+
+        }
+        next();
+    }
+    catch (e) {
+        return res.status(403).json({
+            message: "You are not logged in"
+        })
+
+    }
+
 } 
