f3c6b202b68cc02f98414fb001e7848091d00f9a
akash patel <akash906129@gmail.com>
2025-03-04 00:44:50 +0530

    signup page styling done

--- frontend/app/signup/page.tsx
+++ frontend/app/signup/page.tsx
@@ -1,9 +1,40 @@
+"use client";
+
+import { Input } from "../../components/Input";
+import { PrimaryButton } from "../../components/buttons/PrimaryButton";
+import { CheckFeature } from "../../components/CheckFeature";
 
 export default function Signup() {
     return (
         <div>
-            Signup
+            <div className="flex justify-center">
+                <div className="flex pt-8 max-w-4xl">
+                    <div className=" flex-1 pt-20 px-4">
+                        <div className="font-semibold text-3xl pb-4">
+                            Join millions worldwide who automate their work using Zapier.
+                        </div>
+                        <div className="pb-6 pt-4">
+                            <CheckFeature label="Easy setup, no coding required" />
+                        </div>
+                        <div className="pb-6">
+                            <CheckFeature label="Free forever for core feature" />
+                        </div>
+                        <CheckFeature label={"14-day trial of premium features & apps"} />
+                    </div>
+                    <div className="flex-1 pt-6 pb-6 mt-12 px-4 border rounded mr-4">
+
+                        <Input label="Name" onChange={e => { }} placeholder="Your name" />
+
+                        <Input label="Email" onChange={e => { }} placeholder="Your Email" />
+
+                        <Input label="Password" onChange={e => { }} placeholder="Password" type="password" />
+
+                    </div>
+                </div>
+            </div>
         </div>
     )
 
 }
+
+
